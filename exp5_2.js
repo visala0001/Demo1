e0daba8909c067669fb46b73315c440577ec4875
visala0001 <visaladangeti7@gmail.com>
2026-08-17 10:45:18 +0530

    Update exp5_2.js

--- exp5_2.js
+++ exp5_2.js
@@ -1,33 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 app.get("/", (req, res) => {
-    res.send("Welcome to SVECW Express Server!");
-});
-app.get("/user/:id", (req, res) => {
-    const userId = req.params.id;
-    res.send(`User ID is: ${userId}`);
-});
-app.get("/student/:name/:id", (req, res) => {
-    const name = req.params.name;
-    const id = req.params.id;
-    res.send(`Student Name: ${name}<br>Student ID: ${id}`);
+  res.send("Home Page");
 });
 app.get("/search", (req, res) => {
-    const name = req.query.name;
-    res.send(`Search result for: ${name}`);
+  const name = req.query.name;
+  const age = req.query.age;
+  res.send(`Name: ${name}, Age: ${age}`);
 });
-app.get("/products", (req, res) => {
-    const category = req.query.category;
-    const price = req.query.price;
-    res.send(`Product Category: ${category}<br>Maximum Price: ${price}`);
+app.get("/products/:id", (req, res) => {
+  const productId = req.params.id;
+
+  res.send(`Product ID: ${productId}`);
 });
-app.get("/course/:course", (req, res) => {
-    const course = req.params.course;
-    const year = req.query.year;
-    res.send(`Course: ${course}<br>Year: ${year}`);
-});
-app.listen(PORT, () => {
-    console.log(`Success! Server is running at http://localhost:${PORT}`);
-    console.log("Press Ctrl+C to stop server.");
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
